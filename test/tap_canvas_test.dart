158e985580190d78d242465d9d4fa5665f48d11f
Cillian Myles <myles.cillian@gmail.com>
2021-02-11 20:36:16 +0000

    adding test classes;

--- test/tap_canvas_test.dart
+++ test/tap_canvas_test.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tap_canvas/src/tap_canvas.dart';
+import 'package:tap_canvas/src/tap_offset_provider.dart';
+import 'package:tap_canvas/src/tap_outside_detector_widget.dart';
 
 void main() {
-  group('', () {
+  group('$TapCanvas', () {
+    test('', () {});
+  });
+
+  group('$TapOffsetProvider', () {
+    test('', () {});
+  });
+
+  group('$TapOutsideDetectorWidget', () {
     test('', () {});
   });
 }
