feac9af169a0e60b22505c511d35de77485036c9
Cillian Myles <myles.cillian@gmail.com>
2021-03-14 14:32:30 +0000

    #7 | separate the test cases; make tests more legible/digestible;

--- test/src/tap_offset_provider_test.dart
+++ test/src/tap_offset_provider_test.dart
@@ -1,41 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test_ui/flutter_test_ui.dart';
 import 'package:tap_canvas/src/tap_offset_provider.dart';
 
 void main() {
   group('$TapOffsetProvider', () {
-    Stream<Offset> createStream(int index) => Stream.value(
+    late Stream<Offset> streamOne;
+    late Stream<Offset> streamTwo;
+    late List<TapOffsetProvider> log;
+    late Widget builder;
+
+    Stream<Offset> createStream({required int index}) => Stream.value(
           Offset(
             index.toDouble(),
             index.toDouble(),
           ),
         );
 
-    testWidgets('notifies dependents', (tester) async {
-      final streams = [0, 1].map(createStream).toList(growable: false);
-      final log = <TapOffsetProvider>[];
+    Widget createBuilder({required List<TapOffsetProvider> log}) =>
+        Builder(builder: (context) {
+          log.add(
+            context.dependOnInheritedWidgetOfExactType<TapOffsetProvider>()!,
+          );
+          return Container();
+        });
 
-      final builder = Builder(builder: (context) {
-        log.add(
-          context.dependOnInheritedWidgetOfExactType<TapOffsetProvider>()!,
-        );
-        return Container();
+    TapOffsetProvider createWidget({
+      required Stream<Offset> stream,
+      required Widget child,
+    }) =>
+        TapOffsetProvider(stream: stream, child: child);
+
+    setUpUI((tester) async {
+      streamOne = createStream(index: 1);
+      streamTwo = createStream(index: 2);
+      log = <TapOffsetProvider>[];
+      builder = createBuilder(log: log);
+    });
+
+    group('when a first instance is created', () {
+      late TapOffsetProvider first;
+      late TapOffsetProvider second;
+      late TapOffsetProvider third;
+
+      setUpUI((tester) async {
+        first = createWidget(stream: streamOne, child: builder);
+        await tester.pumpWidget(first);
+        await tester.pump();
       });
 
-      final first = TapOffsetProvider(stream: streams[0], child: builder);
-      await tester.pumpWidget(first);
+      testUI('then the tap offset stream should be accessible by its children',
+          (tester) async {
+        expect(log, equals(<TapOffsetProvider>[first]));
+      });
 
-      expect(log, equals(<TapOffsetProvider>[first]));
+      group('and the widget is rebuilt, but with the same stream input', () {
+        setUpUI((tester) async {
+          second = createWidget(stream: streamOne, child: builder);
+          await tester.pumpWidget(second);
+          await tester.pump();
+        });
 
-      final second = TapOffsetProvider(stream: streams[0], child: builder);
-      await tester.pumpWidget(second);
+        testUI('then the tap offset stream should be un-changed',
+            (tester) async {
+          expect(log, equals(<TapOffsetProvider>[first]));
+        });
+      });
 
-      expect(log, equals(<TapOffsetProvider>[first]));
+      group('and the widget is rebuilt, with a new stream input', () {
+        setUpUI((tester) async {
+          third = createWidget(stream: streamTwo, child: builder);
+          await tester.pumpWidget(third);
+          await tester.pump();
+        });
 
-      final third = TapOffsetProvider(stream: streams[1], child: builder);
-      await tester.pumpWidget(third);
-
-      expect(log, equals(<TapOffsetProvider>[first, third]));
+        testUI('then the tap offset stream should be updated', (tester) async {
+          expect(log, equals(<TapOffsetProvider>[first, third]));
+        });
+      });
     });
   });
 }
